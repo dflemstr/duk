8b5f26331f02f33fc848e324d64d786a128b443a
David Flemström <dflemstr@spotify.com>
2016-07-19 11:27:41 +0200

    Move all duktape_sys logging to trace

--- duktape-sys/src/lib.rs
+++ duktape-sys/src/lib.rs
@@ -23,21 +23,14 @@ pub unsafe extern "C" fn __duktape_sys_debug_write(
     line: libc::c_long,
     func: *const libc::c_char,
     msg: *const libc::c_char) {
-
-    let log_level = if level == DUK_LEVEL_DEBUG {
-        log::LogLevel::Debug
-    } else {
-        log::LogLevel::Trace
-    };
-
-    if log_enabled!(log_level) {
+    if log_enabled!(log::LogLevel::Trace) {
         let file_str = ::std::ffi::CStr::from_ptr(file).to_string_lossy();
 
         let target = format!("{}:{}", module_path!(), file_str);
-        if log_enabled!(target: &target, log_level) {
+        if log_enabled!(target: &target, log::LogLevel::Trace) {
             let func_str = ::std::ffi::CStr::from_ptr(func).to_string_lossy();
             let msg_str = ::std::ffi::CStr::from_ptr(msg).to_string_lossy();
-            log!(target: &target, log_level, "{}.{}:{}: {}", file_str, func_str, line, msg_str);
+            trace!(target: &target, "{}.{}:{}: {}", file_str, func_str, line, msg_str);
         }
     }
 }
