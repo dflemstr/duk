6381544ba48d21f2b75dc6ae9d0bd8dbd7364256
David Flemström <dflemstr@spotify.com>
2016-07-19 16:18:26 +0200

    Explicitly include stdarg.h

--- duktape-sys/examples/gen-wrapper.rs
+++ duktape-sys/examples/gen-wrapper.rs
@@ -46,6 +46,7 @@ fn write_wrapper_header() -> io::Result<()> {
     let mut header_file = try!(fs::File::create("src/wrapper.h"));
 
     try!(writeln!(header_file, "#pragma once"));
+    try!(writeln!(header_file, "#include <stdarg.h>"));
     try!(writeln!(header_file, "#include \"duktape.h\""));
     try!(writeln!(header_file, "#include \"duk_logging.h\""));
 
--- duktape-sys/src/wrapper.h
+++ duktape-sys/src/wrapper.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdarg.h>
 #include "duktape.h"
 #include "duk_logging.h"
 
