41fe0db1c3967c4765dabb7439eb2d42d7268223
Aiden McClelland <me@drbonez.dev>
2022-03-03 17:24:13 -0700

    fix imports when outside of duk crate

--- duk-derive/src/lib.rs
+++ duk-derive/src/lib.rs
@@ -6,6 +6,11 @@ use syn::spanned::Spanned;
 
 #[proc_macro_attribute]
 pub fn duktape_fn(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
+    let duk_path = if std::env::var("CARGO_PKG_NAME").unwrap() == "duk" {
+        quote! { crate }
+    } else {
+        quote! { ::duk }
+    };
     let ast: syn::ItemFn = syn::parse(item.clone()).expect("failed to parse token stream as fn");
     let fn_name = ast.sig.ident;
     let fn_vis = ast.vis;
@@ -24,15 +29,15 @@ pub fn duktape_fn(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
         let arg_idx = args.len() as i32;
         arg_names.push(name_ident.clone());
         args.push(quote! {
-            let #name_ident = match duk::deserialize_from_stack(ctx, #arg_idx) {
+            let #name_ident = match #duk_path::deserialize_from_stack(ctx, #arg_idx) {
                 Ok(a) => a,
                 Err(e) => {
-                    duk::duk_sys::duk_push_error_object(
+                    #duk_path::duk_sys::duk_push_error_object(
                         ctx,
-                        duk::duk_sys::DUK_ERR_TYPE_ERROR as i32,
+                        #duk_path::duk_sys::DUK_ERR_TYPE_ERROR as i32,
                         std::ffi::CString::new(format!("{}", e)).unwrap_or_default().into_raw(),
                     );
-                    duk::duk_sys::duk_throw_raw(ctx);
+                    #duk_path::duk_sys::duk_throw_raw(ctx);
                     return 0;
                 }
             };
@@ -41,17 +46,17 @@ pub fn duktape_fn(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
     let fn_arg_len = args.len();
     let ret = match ast.sig.output {
         syn::ReturnType::Type(_, _) => quote! {
-            match duk::serialize_to_stack(ctx, &res) {
+            match #duk_path::serialize_to_stack(ctx, &res) {
                 Ok(_) => {
                     1
                 },
                 Err(e) => {
-                    duk::duk_sys::duk_push_error_object(
+                    #duk_path::duk_sys::duk_push_error_object(
                         ctx,
-                        duk::duk_sys::DUK_ERR_TYPE_ERROR as i32,
+                        #duk_path::duk_sys::DUK_ERR_TYPE_ERROR as i32,
                         std::ffi::CString::new(format!("{}", e)).unwrap_or_default().into_raw(),
                     );
-                    duk::duk_sys::duk_throw_raw(ctx);
+                    #duk_path::duk_sys::duk_throw_raw(ctx);
                     0
                 }
             }
@@ -62,15 +67,14 @@ pub fn duktape_fn(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
     };
     let gen = quote! {
         #fn_vis mod #fn_name {
-            use super::duk;
             use super::#fn_name as fn_impl;
 
             pub struct DukFnImpl;
 
-            unsafe impl duk::DukFunction for DukFnImpl {
+            unsafe impl #duk_path::DukFunction for DukFnImpl {
                 const NARGS: usize = #fn_arg_len;
                 const NAME: &'static str = #fn_name_str;
-                unsafe extern "C" fn duk_call(ctx: *mut duk_sys::duk_context) -> i32 {
+                unsafe extern "C" fn duk_call(ctx: *mut #duk_path::duk_sys::duk_context) -> i32 {
                     #(
                         #args
                     )*
@@ -86,12 +90,12 @@ pub fn duktape_fn(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
                             } else {
                                 "panic: unknown error".into()
                             };
-                            duk::duk_sys::duk_push_error_object(
+                            #duk_path::duk_sys::duk_push_error_object(
                                 ctx,
-                                duk::duk_sys::DUK_ERR_ERROR as i32,
+                                #duk_path::duk_sys::DUK_ERR_ERROR as i32,
                                 std::ffi::CString::new(error).unwrap_or_default().into_raw(),
                             );
-                            duk::duk_sys::duk_throw_raw(ctx);
+                            #duk_path::duk_sys::duk_throw_raw(ctx);
                             return 0;
                         }
                     };
